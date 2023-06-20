0f6819d7cc7be3450d32d3d6db9935515777a4ed
Zeha Irawan <zehairawan@gmail.com>
2023-06-20 08:49:09 +0800

    fetch from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import "./App.css";
 import InfoCard from "./components/InfoCard/InfoCard";
 import RadioButton from "./components/RadioButton/RadioButton";
-import TembBar from "./components/TempBar/TempBar";
-import TembarGroup from "./components/TempBarGroup/TembarGroup";
-import weatherData from "./mockup/data.json";
-import { useState } from "react";
+import TempBar from "./components/TempBar/TempBar";
+import TempBarGroup from "./components/TempBarGroup/TempBarGroup";
+import { useState, useEffect } from "react";
+import axios, { AxiosResponse } from "axios";
 
-function App() {
+interface WeatherDataItem {
+  weather: {
+    main: string;
+  }[];
+  main: {
+    temp: number;
+  };
+  dt_txt: string;
+}
+
+function App(): JSX.Element {
   const [currentDate, setCurrentDate] = useState(new Date());
+  const [weatherData, setWeatherData] = useState<WeatherDataItem[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [filteredWeatherData, setFilteredWeatherData] = useState<
+    WeatherDataItem[]
+  >([]);
 
   const previousDate = () => {
     const newDate = new Date(currentDate);
@@ -21,9 +36,41 @@ function App() {
     setCurrentDate(newDate);
   };
 
-  const filteredWeatherData = weatherData.list.filter(
-    (item) => item.dt_txt.slice(8, 10) === currentDate.getDate().toString(),
-  );
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const response: AxiosResponse = await axios.get(
+          "http://api.openweathermap.org/data/2.5/forecast",
+          {
+            params: {
+              q: "Kathmandu",
+              appId: "11161feb64ba65936332e6341f7b2d06",
+              cnt: 40,
+              units: "metric",
+            },
+          },
+        );
+        setWeatherData(response.data.list);
+      } catch (error) {
+        console.log("Error fetching weather data:", error);
+      }
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    setFilteredWeatherData(
+      weatherData.filter(
+        (item: WeatherDataItem) =>
+          item.dt_txt.slice(8, 10) === currentDate.getDate().toString(),
+      ),
+    );
+  }, [currentDate, weatherData]);
+
+  console.log(weatherData, "weatherData");
 
   const [selectedOption, setSelectedOption] = useState<string>("celcius");
 
@@ -36,6 +83,9 @@ function App() {
     const temperature = item.main.temp;
     return temperature > maxTemp ? temperature : maxTemp;
   }, -Infinity);
+
+  if (isLoading || filteredWeatherData.length === 0)
+    return <div>Loading...</div>;
 
   return (
     <>
@@ -50,20 +100,18 @@ function App() {
         nextDate={nextDate}
         weatherData={filteredWeatherData[0]}
       />
-      <TembarGroup>
-        {filteredWeatherData.map((item) => {
-          return (
-            <TembBar
-              lowestTemp={lowestTemp}
-              highestTemp={highestTemp}
-              key={item.dt}
-              weatherData={item}
-              selectedOption={selectedOption}
-            />
-          );
-        })}
-      </TembarGroup>
-      {/* <div className="overlay"></div> */}
+
+      <TempBarGroup>
+        {filteredWeatherData.map((item) => (
+          <TempBar
+            lowestTemp={lowestTemp}
+            highestTemp={highestTemp}
+            key={item.dt_txt}
+            weatherData={item}
+            selectedOption={selectedOption}
+          />
+        ))}
+      </TempBarGroup>
     </>
   );
 }
